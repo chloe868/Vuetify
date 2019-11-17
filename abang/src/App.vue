f8623e85c6f7f5bb9b6c95b87a0788b50cc666a8
chloe868 <cherrymae.herrera@student.passerellesnumeriques.org>
2019-11-17 19:26:21 +0800

    updated

--- abang/src/App.vue
+++ abang/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <!-- <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -31,26 +31,21 @@
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
-    </v-app-bar>
-
+    </v-app-bar> -->
     <v-content>
-      <DatePickerModal />
+      <router-view/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import DatePickerModal from "./views/Login";
+// import FirstPage from "./components/FirstPage";
 
 export default {
   name: "App",
 
-  components: {
-    DatePickerModal
-  },
-
-  data: () => ({
-    //
-  })
+  // data: () => ({
+  //   //
+  // })
 };
 </script>
